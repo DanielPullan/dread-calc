5adbbacc5bb803c489fcbf2de6b2dffa7b32099a
Daniel Pullan <dan@3264.uk>
2025-06-03 20:53:45 +0100

    Working version
    Basic working version

--- README.md
+++ README.md
@@ -9,7 +9,13 @@ DREAD Calc is a web based tool that can be used to complete a risk assessment fo
 
 DREAD is a risk assessment model. It's made up of 5 components:
 - Damage: How much damage could an attack cause.
-- Reproducability: How easily can the same attack be repeated.
+- Reproducibility: How easily can the same attack be repeated.
 - Exploitability: How difficult is it to actually launch an attack.
 - Affected Users: How many assets are affected by the attack.
 - Discoverability: How easily can the vulnerability be discovered.
+
+It was developed by Microsoft in 2002 and discontinued in 2008 due to the ratings being inconsistent and too subjective.
+
+### Why develop DREAD Calc?
+
+DREAD Calc is for educational purposes. I would also like to develop STRIDE Calc for the same purpose using the STRIDE model.
--- app.py
+++ app.py
@@ -3,14 +3,39 @@ from flask import Flask, request, render_template, make_response, redirect, url_
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
 	title = "Home"
-	return render_template("index.html", data=secrets.choice(quotes), title=title)
+	data = "Data"
 
-@app.route('/api')
-def api(METHODS='get'):
-	return secrets.choice(quotes)
+# DREAD is a risk assessment model. It's made up of 5 components:
+# - Damage: How much damage could an attack cause.
+# - Reproducibility: How easily can the same attack be repeated.
+# - Exploitability: How difficult is it to actually launch an attack.
+# - Affected Users: How many assets are affected by the attack.
+# - Discoverability: How easily can the vulnerability be discovered.
+
+	if request.method == 'POST':
+		form_damage = int(request.form['damage'])
+		form_reproducibility = int(request.form['reproducibility'])
+		form_exploitability = int(request.form['exploitability'])
+		form_affectedUsers = int(request.form['affectedUsers'])
+		form_discoverability = int(10) # Always treated as 10
+
+		print(form_damage)
+		print(form_reproducibility)
+		print(form_exploitability)
+		print(form_affectedUsers)
+		print(form_discoverability)
+
+		dread_score = int((form_damage + form_reproducibility + form_exploitability + form_affectedUsers
+			+ form_discoverability) / 5)
+
+		print(dread_score)
+
+		return render_template("result.html", title=title, data=data, dread_score=dread_score)
+
+	return render_template("index.html", title=title, data=data)
 
 @app.errorhandler(404)
 def page_not_found(e):
